308db6a28d0dd8454cf051be47194a6a9631484d
Xavier Stouder <xavier@stouder.io>
2022-12-01 16:34:08 +0100

    feat: day 1

---
+++ src/day1.js
@@ -0,0 +1,14 @@
+import { open } from 'node:fs/promises';
+
+const file = await open(new URL('../inputs/day1.txt', import.meta.url), 'r');
+
+const elvesCalories = []
+for await (const line of file.readLines()) {
+  if(line === '') elvesCalories.push(0)
+  else elvesCalories[elvesCalories.length - 1] += parseInt(line)
+}
+
+const reversedElves = elvesCalories.sort((a, b) => b - a)
+
+console.log(`Part 1: ${reversedElves[0]}`)
+console.log(`Part 2: ${reversedElves.splice(0, 3).reduce((a, b) => a + b)}`)
